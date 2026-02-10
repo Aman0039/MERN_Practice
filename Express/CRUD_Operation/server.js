ea2bce22e43b041ef5e589b96232c88d7716b655
Aman0039 <pathakaman0532gmail.com>
2026-02-11 02:37:04 +0530

    db.json added and update server.js

--- Express/CRUD_Operation/server.js
+++ Express/CRUD_Operation/server.js
@@ -2,21 +2,37 @@
 
 const express = require("express");
 
+//import fs module
+
+const fs = require("fs")
+
 //Calling Express in app
 const app = express();
 
+app.use(express.json()) //JSON body parser
+
 //Get Method
 app.get("/" , (req,res)=>{
-    res.send("This is the Home Route");
+    res.json({msg:"This is the Home Route"});
 })
+
+app.get("/all-courses" , (req,res)=>{
+
+    // Data in the form of Stringify;
+    // let data = fs.readFileSync("./db.json" , "utf-8");
+
+    //parse the data into JSON
+    let data = JSON.parse(fs.readFileSync("./db.json" , "utf-8"));
+    let courses = data.courses
+    console.log(data.courses)
+    res.json({msg : "List of All Courses" , courses})
+})
+
+
+
 
 //Port Created.
 const PORT = 4000;
-
-
-
-
-
 //Server Start
 app.listen(PORT , ()=>{
     console.log(`Server is Started on http://localhost:${PORT}`);
